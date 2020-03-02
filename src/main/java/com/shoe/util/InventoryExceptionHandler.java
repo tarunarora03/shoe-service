bc1cdd146c86ff122ec81e322ca1767c3a110bb4
tarunarora03 <tarunarora03@gmail.com>
2020-03-01 21:43:19 -0500

    Update InventoryExceptionHandler.java

--- src/main/java/com/shoe/util/InventoryExceptionHandler.java
+++ src/main/java/com/shoe/util/InventoryExceptionHandler.java
@@ -12,10 +12,8 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@EnableWebMvc
 @ControllerAdvice
 public class InventoryExceptionHandler extends ResponseEntityExceptionHandler {
 
